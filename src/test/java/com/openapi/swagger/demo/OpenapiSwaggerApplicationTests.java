8d43eb0aaec956b63e81c09af1c20f723daa0de9
unknown <kunal.scjp@gmail.com>
2022-04-28 16:45:23 +0530

    first open api commit

---
+++ src/test/java/com/openapi/swagger/demo/OpenapiSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapi.swagger.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
